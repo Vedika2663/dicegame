adcffd6290833ec6dc819344e53845bd91983e41
Vedika2663 <94191970+Vedika2663@users.noreply.github.com>
2023-05-24 02:04:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var num1 = Math.floor(Math.random()*6) + 1;
-var diceImage="images/"+"dice"+num1+".png";
-//var value=images/dice1.png;
+var diceImage="dice"+num1+".png";
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",diceImage);
 var num2 = Math.floor(Math.random()*6) + 1;
-var diceImage1="images/"+"dice"+num2+".png";
-//var value=images/dice1.png;
+var diceImage1="dice"+num2+".png";
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",diceImage1);
 if(num1>num2){
