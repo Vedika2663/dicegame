e0f363cb783d115906d95469447c9b325688d705
Vedika2663 <94191970+Vedika2663@users.noreply.github.com>
2023-05-24 02:03:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var num1 = Math.floor(Math.random()*6) + 1;
+var diceImage="images/"+"dice"+num1+".png";
+//var value=images/dice1.png;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",diceImage);
+var num2 = Math.floor(Math.random()*6) + 1;
+var diceImage1="images/"+"dice"+num2+".png";
+//var value=images/dice1.png;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",diceImage1);
+if(num1>num2){
+    document.querySelector("h1").innerHTML="Player1 wins";
+}else if(num1===num2){
+    document.querySelector("h1").innerHTML="Draw";
+}else{
+    document.querySelector("h1").innerHTML="Player2 wins";
+}
